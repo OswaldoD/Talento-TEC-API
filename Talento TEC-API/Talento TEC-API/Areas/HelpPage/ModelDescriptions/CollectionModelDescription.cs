ac37f7e254ff1603013d13bda3b641a736759f07
OswaldoD <dreammicro7@hotmail.com>
2016-09-10 21:57:42 -0600

    Inicio de API

---
+++ Talento TEC-API/Talento TEC-API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Talento_TEC_API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Talento TEC-API/Talento TEC-API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Talento_TEC_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
